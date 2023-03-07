20caf72de5749eda7d4aa11a99b8895d8136cebb
EdiMarian <ichimedimarian@gmail.com>
2023-03-07 18:09:38 +0200

    feature: NavBar add

--- src/components/Layout/Navbar/index.tsx
+++ src/components/Layout/Navbar/index.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import {
-  faChartSimple,
-  faFileSignature
+  faBank,
+  faBuilding,
+  faCartShopping,
+  faPager,
+  faRightLeft
 } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useGetIsLoggedIn } from '@multiversx/sdk-dapp/hooks';
 import { logout } from '@multiversx/sdk-dapp/utils';
-import { Navbar as BsNavbar, NavItem, Nav } from 'react-bootstrap';
+import { Navbar as BsNavbar, NavItem, Nav, NavbarBrand } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import { dAppName } from 'config';
 import { routeNames } from 'routes';
-import { ReactComponent as MultiversXLogo } from '../../../assets/img/multiversx.svg';
+import { ReactComponent as Logo } from '../../../assets/img/logo.svg';
 
 export const Navbar = () => {
   const isLoggedIn = useGetIsLoggedIn();
@@ -20,28 +22,46 @@ export const Navbar = () => {
   };
 
   return (
-    <BsNavbar className='bg-white border-bottom px-4 py-3'>
-      <div className='container-fluid'>
-        <Link
-          className='d-flex align-items-center navbar-brand mr-0'
-          to={isLoggedIn ? routeNames.dashboard : routeNames.home}
-        >
-          <MultiversXLogo className='multiversx-logo' />
-          <span className='dapp-name text-muted'>{dAppName}</span>
-        </Link>
-
-        <Nav className='ml-auto'>
-          {isLoggedIn && (
-            <>
+    <>
+      <BsNavbar>
+        <div className='container-fluid px-0 px-md-5 d-flex align-items-start'>
+          <Nav className='py-3'>
+            {isLoggedIn && (
+              <>
+                <NavItem>
+                  <Link className='navItem-link' to={routeNames.dashboard}>Dashboard</Link>
+                </NavItem>
+              </>
+            )}
+          </Nav>
+          <NavbarBrand><Logo className='logo'/></NavbarBrand>
+          <Nav className='py-3'>
+            {isLoggedIn && (
+              <>
+                <NavItem>
+                  <p className='navItem-link' onClick={handleLogout}>
+                    Logout
+                  </p>
+                </NavItem>
+              </>
+            )}
+            {!isLoggedIn && (
               <NavItem>
-                <button className='btn btn-link' onClick={handleLogout}>
-                  Close
-                </button>
+                <Link className='navItem-link' to={routeNames.unlock}>
+                  Connect
+                </Link>
               </NavItem>
-            </>
-          )}
-        </Nav>
-      </div>
-    </BsNavbar>
+            )}
+          </Nav>
+        </div>
+      </BsNavbar>
+      <Nav className='d-block mx-auto d-sm-flex mx-sm-0 justify-content-center pt-2'>
+        <NavItem className='subNavItem-link'><FontAwesomeIcon icon={faRightLeft} /> Swap</NavItem>
+        <NavItem className='subNavItem-link'><FontAwesomeIcon icon={faBuilding} /> My Castle</NavItem>
+        <NavItem className='subNavItem-link'><FontAwesomeIcon icon={faCartShopping} /> Shop</NavItem>
+        <NavItem className='subNavItem-link'><FontAwesomeIcon icon={faBank} /> Banks</NavItem>
+        <NavItem className='subNavItem-link'><FontAwesomeIcon icon={faPager} /> Secondary Market</NavItem>
+      </Nav>
+    </>
   );
 };
