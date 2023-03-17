0681d9b9a553f15205469624cd2c172e7647fefc
EdiMarian <ichimedimarian@gmail.com>
2023-03-17 19:21:37 +0200

    feature: loadSfts & fetchSfts add

--- src/store/AccountStore.ts
+++ src/store/AccountStore.ts
@@ -1,10 +1,24 @@
-import { makeAutoObservable } from "mobx";
+import { makeAutoObservable, runInAction, action } from "mobx";
 import AccountService from './services/AccountService';
+import { NftType } from '@multiversx/sdk-dapp/types/tokens.types';
 
 class AccountStore {
-    constructor(private readonly accountService: AccountService){
+    sfts: NftType[];
+    constructor(public readonly accountService: AccountService){
         makeAutoObservable(this);
+        this.accountService = accountService;
+        this.sfts = [];
     }
+    
+    async loadSfts(address: string): Promise<void> {
+        const sftsFound: NftType[] = await this.accountService.fetchSfts(address);
+
+        if(sftsFound.length === 0) return;
+        runInAction(() => {
+            this.sfts = sftsFound;
+        })
+    }
+
 }
 
 const account = new AccountStore(new AccountService());
--- src/store/services/AccountService.ts
+++ src/store/services/AccountService.ts
@@ -1,5 +1,18 @@
+import { NftType } from '@multiversx/sdk-dapp/types/tokens.types';
+import axios from 'axios';
+import { API_URL, COLLECTION_IDENTIFIER } from '../../config';
 class AccountService {
     constructor(){}
+
+    async fetchSfts(address: string): Promise<NftType[]> {
+        try {
+            const { data } = await axios.get(API_URL + '/accounts/' + address + '/nfts?collection=' + COLLECTION_IDENTIFIER);
+            return data;
+        } catch (err) {
+            console.log(err)
+            return [];
+        }
+    }
 }
 
 export default AccountService;
