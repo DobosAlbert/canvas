8595ce832f0014fbd1f2f6f5519746f07a617982
EdiMarian <ichimedimarian@gmail.com>
2023-03-24 17:23:39 +0200

    home page edited

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,36 +1,15 @@
-import * as React from 'react';
-import { Link } from 'react-router-dom';
 import { AuthRedirectWrapper } from 'components';
 import { dAppName } from 'config';
-import { routeNames } from 'routes';
+import { routeNames } from '../routes';
+import { Link } from 'react-router-dom';
 
 const HomePage = () => {
   return (
-    <div className='d-flex flex-fill align-items-center container'>
-      <div className='row w-100'>
-        <div className='col-12 col-md-8 col-lg-5 mx-auto'>
-          <div className='card shadow-sm rounded p-4 border-0'>
-            <div className='card-body text-center'>
-              <h2 className='mb-3' data-testid='title'>
-                {dAppName}
-              </h2>
-
-              <p className='mb-3'>
-                This is an MultiversX dapp sample.
-                <br /> Login using your MultiversX wallet.
-              </p>
-
-              <Link
-                to={routeNames.unlock}
-                className='btn btn-primary mt-3 text-white'
-                data-testid='loginBtn'
-              >
-                Login
-              </Link>
-            </div>
-          </div>
-        </div>
-      </div>
+    <div className='container'>
+      <h2 className='my-5 text-center' data-testid='title'>
+        {dAppName}
+      </h2>
+      <Link to={routeNames.unlock} className="text-dark text-center text-decoration-none"><h5>Connect with your wallet!</h5></Link>
     </div>
   );
 };
